dc0c7a7b27479c8213e61b9c15dd773640c7d8c3
Xuân Cường Đỗ <exidae7@Xuans-MacBook-Pro.local>
2022-09-18 13:39:50 +0700

    simple contract

--- deploy.js
+++ deploy.js
@@ -1,1 +1,31 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./compile')
+require('dotenv').config()
+let PRIVATE_KEY = process.env.PRIVATE_KEY
+let RINKEBY_INFURA = process.env.RINKEBY_INFURA
+
+
+const provider = new HDWalletProvider(
+     PRIVATE_KEY,
+     RINKEBY_INFURA
+)
+const web3 = new Web3(provider)
+
+
+const deploy = async () => {
+     const account = await web3.eth.getAccounts()
+     console.log('attempting to deploy' , account[0])
+
+     const result = await new web3.eth.Contract(JSON.parse(interface))
+     .deploy({
+          data : bytecode , arguments :  ['Hello']          
+     })
+     .send({gas : '1000000' , from : account[0]})
+
+     console.log('contract deployed to ' , result.options.address)
+     provider.engine.stop()
+}
+
+
+deploy()
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,39 @@
-// contract test code will go here
+const assert = require('assert')
+const ganache = require('ganache')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+const { interface , bytecode } = require('../compile')
+
+
+
+let accounts
+let inbox
+
+beforeEach(async () => {
+     // get list of all account
+     accounts = await web3.eth.getAccounts()
+     
+     // use one of those account to deploy
+     // contracts
+     inbox = await new web3.eth.Contract(JSON.parse(interface))
+     .deploy({data : bytecode , arguments: ['Hi there'] })
+     .send({ from : accounts[0] , gas : '1000000'})
+
+})
+
+describe('Inbox' , () => {
+     it('deploys a contract' , () => {
+          assert.ok(inbox.options.address)
+     })
+     it('has a default messsage' , async () => {
+          const message = await   inbox.methods.message().call()
+          assert.equal(message , 'Hi there')
+     })
+     it('can change the message' , async () =>{
+          await inbox.methods.setMessage('bye').send({
+               from : accounts[0]
+          })
+          const message = await   inbox.methods.message().call()
+          assert.equal(message , 'bye')
+     })
+})
